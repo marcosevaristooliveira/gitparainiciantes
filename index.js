daf7c06d8176c5c47038ad3da39d205c765ac8ae
Marcos Evaristo Oliveira Costa <marcosevaristooliveira@gmail.com>
2021-03-05 05:06:05 -0300

    Nova rota adicionada

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get('/', (req, res)=>{
     res.status(200).send({message: 'Olá mmundo esse é o meu novo repositório clonado.'})
 })
 
+app.get('Ola/', (req, res)=>{
+    res.status(200).send({message: 'Olá'})
+})
+
 app.listen(4001, ()=>{
     console.log('Api inicializda na porta 4001');
 })
